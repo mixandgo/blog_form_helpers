40ce4e53e70026b84840afc37277ce459c15454b
Cezar Halmagean <cezar@mixandgo.com>
2015-10-06 12:08:06 +0300

    Add helper code

--- app/helpers/custom_form_builder.rb
+++ app/helpers/custom_form_builder.rb
@@ -8,13 +8,27 @@ class CustomFormBuilder
     @options = options
   end
 
-  def user_name_field(options={})
-    content_tag :div, class: "user-fullname" do
-      content_tag :input, nil, type: "text", value: @user.name
-    end
+  def user_fields
+    user_name_field +
+    user_email_field
   end
 
+  # This is required
   def multipart?
   end
-end
 
+  private
+    def user_name_field(options={})
+      content_tag :div, class: "user-name" do
+        content_tag(:label, "Name: ", for: :name) +
+        content_tag(:input, nil, type: "text", value: @user.name)
+      end
+    end
+
+    def user_email_field(options={})
+      content_tag :div, class: "user-email" do
+        content_tag(:label, "Email: ", for: :email) +
+        content_tag(:input, nil, type: "text", value: @user.email)
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resource :user
+  resource :users
   root 'site#index'
 end
